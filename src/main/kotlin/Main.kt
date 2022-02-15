45ba4746803c81e68a6089b6b9983ad77ba66732
wangare <marywangareee@gmail.com>
2022-02-15 22:24:50 +0300

    my records

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var name: String=("Bakhita Ismail")
+    var age: Byte=21
+    var phonenumber: String=("0768658959")
+    var weight_in_kg=("67")
+    var kenyan:Boolean=true
+
+    println(name)
+    println(age)
+    println(phonenumber)
+    println(weight_in_kg)
+    println(kenyan)
+
+
+}
+
